5e3202e15333f6cab9173ded2717532272b7e34f
TinoSegatti <valentinosegatti@gmail.com>
2025-08-15 16:41:26 -0300

    cambio url next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ const nextConfig = {
   reactStrictMode: true,
   // Configuración básica para Vercel
   async rewrites() {
-    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'https://back-ecolink.onrender.com';
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'https://ecolink-backend.onrender.com';
     
     // Asegúrate de que la URL no termine con /
     const formattedApiUrl = apiUrl.endsWith('/') ? apiUrl.slice(0, -1) : apiUrl;
